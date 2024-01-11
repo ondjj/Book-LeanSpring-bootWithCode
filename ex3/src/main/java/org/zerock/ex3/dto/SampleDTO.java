5685e674e8afde012142a6c9935f9d1ec36173ae
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-11 20:49:21 +0900

    SpringWithCode(part1) : JPA, Thymeleaf 개요

---
+++ ex3/src/main/java/org/zerock/ex3/dto/SampleDTO.java
@@ -0,0 +1,18 @@
+package org.zerock.ex3.dto;
+
+import java.time.LocalDateTime;
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder(toBuilder = true)
+public class SampleDTO {
+
+    private Long sno;
+
+    private String first;
+
+    private String last;
+
+    private LocalDateTime regTime;
+}
