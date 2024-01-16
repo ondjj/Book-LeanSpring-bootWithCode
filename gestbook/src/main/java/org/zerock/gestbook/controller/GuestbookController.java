fdb72a7e49c55d8a56fc4c8650b697e7fc59bef0
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-01-16 19:59:02 +0900

    feat(page-get-post) : 목록 페이지 조회 / 등록 처리

--- gestbook/src/main/java/org/zerock/gestbook/controller/GuestbookController.java
+++ gestbook/src/main/java/org/zerock/gestbook/controller/GuestbookController.java
@@ -1,18 +1,42 @@
 package org.zerock.gestbook.controller;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.log4j.Log4j2;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.zerock.gestbook.dto.GuestbookDTO;
+import org.zerock.gestbook.dto.PageRequestDTO;
+import org.zerock.gestbook.service.GuestbookService;
 
 @Controller
 @RequestMapping("/guestbook")
 @Log4j2
+@RequiredArgsConstructor
 public class GuestbookController {
 
-    @GetMapping({"/", "/list"})
-    public String list() {
-        log.info("list.............");
-        return "/guestbook/list";
+    private final GuestbookService service;
+
+    @GetMapping("/list")
+    public void list(@ModelAttribute("pageRequestDTO") PageRequestDTO pageRequestDTO, Model model) {
+        log.info("list............." + pageRequestDTO);
+        model.addAttribute("result", service.getList(pageRequestDTO));
+    }
+
+    @GetMapping("/register")
+    public void register() {
+        log.info("register get ...");
+    }
+
+    @PostMapping("/register")
+    public String registerPost(GuestbookDTO dto, RedirectAttributes redirectAttributes) {
+        log.info("dto ..." + dto);
+        Long gno = service.register(dto);
+        redirectAttributes.addFlashAttribute("msg", gno);
+        return "redirect:/guestbook/list";
     }
 }
