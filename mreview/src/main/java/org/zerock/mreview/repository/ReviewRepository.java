8d189d5f403949b70c953566aac0e4fc233eed60
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-02-16 16:50:24 +0900

    feat(review) : @EntityGraph 적용 -> 명시한 속성은 EAGER로 처리할 수 있음

--- mreview/src/main/java/org/zerock/mreview/repository/ReviewRepository.java
+++ mreview/src/main/java/org/zerock/mreview/repository/ReviewRepository.java
@@ -1,7 +1,14 @@
 package org.zerock.mreview.repository;
 
+import java.util.List;
+import org.springframework.data.jpa.repository.EntityGraph;
+import org.springframework.data.jpa.repository.EntityGraph.EntityGraphType;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.zerock.mreview.entity.Movie;
 import org.zerock.mreview.entity.Review;
 
 public interface ReviewRepository extends JpaRepository<Review, Long> {
+
+    @EntityGraph(attributePaths = {"member"}, type = EntityGraphType.FETCH)
+    List<Review> findByMovie(Movie movie);
 }
--- mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/ReviewRepositoryTests.java
@@ -2,6 +2,7 @@ package org.zerock.mreview.repository;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -32,6 +33,19 @@ class ReviewRepositoryTests {
                     .build();
 
             reviewRepository.save(movieReview);
+        });
+    }
+
+    @Test
+    public void testGetMovieReviews() {
+        Movie movie = Movie.builder().mno(92L).build();
+        List<Review> result = reviewRepository.findByMovie(movie);
+        result.forEach(movieReview -> {
+            System.out.println(movieReview.getReviewnum());
+            System.out.println("\t" + movieReview.getGrade());
+            System.out.println("\t" + movieReview.getText());
+            System.out.println("\t" + movieReview.getMember().getEmail());
+            System.out.println("--------------------------------");
         });
     }
 }
