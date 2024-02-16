f20132e73693b4468e09d458de6b73532ed7e475
Chang-Jun Jeon <jcjk0302@gmail.com>
2024-02-16 16:58:01 +0900

    feat(member-review) : FK 관계 설정에 따라 삭제 순서 주의 필요, 효율적인 쿼리를 위해 삭제, 업데이트에는 @Modifying 어노테이션 반드시 필요, query도 직접 작성하는게 질의가 한 번 발생해서 좋음

--- mreview/src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
+++ mreview/src/test/java/org/zerock/mreview/repository/MemberRepositoryTests.java
@@ -6,12 +6,17 @@ import java.util.stream.IntStream;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Commit;
+import org.springframework.transaction.annotation.Transactional;
 import org.zerock.mreview.entity.Member;
 
 @SpringBootTest
 class MemberRepositoryTests {
     @Autowired
     private MemberRepository memberRepository;
+
+    @Autowired
+    private ReviewRepository reviewRepository;
 
     @Test
     public void insertMember() {
@@ -22,5 +27,15 @@ class MemberRepositoryTests {
                     .nickname("reviewer" + i).build();
             memberRepository.save(member);
         });
+    }
+
+    @Commit
+    @Transactional
+    @Test
+    public void testDeleteMember() {
+        Long mid = 1L;
+        Member member = Member.builder().mid(mid).build();
+        reviewRepository.deleteByMember(member);
+        memberRepository.deleteById(mid);
     }
 }
